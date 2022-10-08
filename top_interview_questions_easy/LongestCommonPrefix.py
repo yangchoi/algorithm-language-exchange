804bf674f1e45864c955a7fc95919781ede1ee53
YangChoi <yang.hj.r@gmail.com>
2022-10-09 07:12:14 +0900

    longest common prefix - edit

--- top_interview_questions_easy/LongestCommonPrefix.py
+++ top_interview_questions_easy/LongestCommonPrefix.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def longestCommonPrefix(self, strs):
         if not strs:
             return ""
@@ -7,9 +7,9 @@ class Solution(object):
         # 튜플 형태로 차례로 접근할 수 있는 반복자 반환
         for i, letter_group in enumerate(zip(*strs)):
           # *변수: 해당 매개변수를 가변적인 갯수를 가진 위치 인수로 정의하겠다는 것
-          참고: https://hcnoh.github.io/2019-01-27-python-arguments-asterisk
-            if len(set(letter_group)) >1:
-                return strs[0][:1]
-            else:
-                return min(strs)
+          # 참고: https://hcnoh.github.io/2019-01-27-python-arguments-asterisk
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
         

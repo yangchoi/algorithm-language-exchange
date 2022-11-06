11d040a8184aba3bf65cef5669230b9f140e23c9
양효정 <48708746+yangchoi@users.noreply.github.com>
2022-11-06 15:22:20 +0900

    Time: 3171 ms (7.88%), Space: 25 MB (38.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        day_num = len(prices)
-        output = 0
-        min_price = sys.maxsize
-
-        for i in range(day_num):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            output = max(output, prices[i] - min_price)
-        return output
+        min_price = 10000 # 임의의 최저 주식값
+        profit = 0 # 수익
+        
+        for current_price in prices:
+            min_price = min(current_price, min_price)
+            profit = max(profit, current_price - min_price)
+        return profit
+            

ec2280da71dcd58398ec9bf57a43ad5c6e3ed367
양효정 <48708746+yangchoi@users.noreply.github.com>
2022-11-08 04:46:17 +0900

    Time: 296 ms (57.18%), Space: 16.7 MB (50.58%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
     
-        dict_ = {}
-        for i in nums:
-            if i not in dict_:
-                dict_[i] = 1
-            else:
-                dict_[i] += 1
-        return min(dict_, key=dict_.get)    
+        answer = 0
+        for num in nums:
+            answer = answer^num # ^ -> XOR
+        return answer 

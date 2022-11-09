e607b05b9cc114cb2eb493247facf14710ad3095
양효정 <48708746+yangchoi@users.noreply.github.com>
2022-11-10 00:08:29 +0900

    Time: 73 ms (27.60%), Space: 13.9 MB (15.43%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+        
+        while True:
+            result = 0
+            for x in str(n):
+                visited.add(int(x))
+                result += int(x) * int(x)
+            
+            if result == 1:
+                return True
+            if result in visited:
+                return False
+            n = result

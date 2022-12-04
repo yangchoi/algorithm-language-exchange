06202579f3aacfbf3654827bb5739958feed11c9
양효정 <48708746+yangchoi@users.noreply.github.com>
2022-12-05 05:41:29 +0900

    Time: 152 ms (36.99%), Space: 15.4 MB (9.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,8 @@
-"""
-approach
-큰 for문을 돌면서 한 정수에 대해 다른 값들을 맞추어 가며 합이 taget이 되는지 확인한다.
-
-"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-            dict = {}
-            for i in range(len(nums)):
-                if nums[i] in dict:
-                    return [dict[nums[i]], i]
-                else:
-                    dict[target - nums[i]] = i
-
-
-
+        hash_table = {}
+        for i in range(len(nums)):
+            if nums[i] in hash_table:
+                return [hash_table[nums[i]], i]
+            else:
+                hash_table[target - nums[i]] = i
